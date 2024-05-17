baa4aee3dd4f5aacf4f15c2cebc19f5cb94d7eea
Greg Dennis <gregsdennis@yahoo.com>
2024-05-17 17:12:24 +1200

    add google analytics

--- run.bat
+++ run.bat
@@ -1,5 +1,13 @@
 @echo off
 del /s /q _site >nul
+
+if "%1" == "prod" (
+  set JEKYLL_ENV=production
+  echo running prod
+) else (
+  set JEKYLL_ENV=
+)
 @echo on
 
 bundle exec jekyll serve --incremental
+
